7a1a6ea6c7c5612026461d7eef0b116e87c1fcfa
John Hall <jkhall@wpi.edu>
2024-11-26 11:55:02 -0500

    updated body list

--- OpenManip_Skeleton-main/src/lab1_base.m
+++ OpenManip_Skeleton-main/src/lab1_base.m
@@ -3,6 +3,8 @@ travelTime = 2; % Defines the travel time
 robot = Robot(); % Creates robot object
 robot.writeTime(travelTime); % Write travel time
 robot.writeMotorState(true); % Write position mode
+addpath("~/RBE501/ModernRobotics-master/packages/MATLAB/mr");
+
 %% Program 
 
 robot.writeJoints(0); % Write joints to zero position
