54ef97047b36fafe2acad090fc9931323e7f71b5
riblair <rileypatblair@gmail.com>
2024-12-02 19:17:02 -0500

    task 1 started

--- OpenManip_Skeleton-main/src/Robot.m
+++ OpenManip_Skeleton-main/src/Robot.m
@@ -1,13 +1,19 @@
 % (c) 2023 Robotics Engineering Department, WPI
 % Skeleton Robot class for OpenManipulator-X Robot for RBE 3001
-
 classdef Robot < OM_X_arm
+    
     % Many properties are abstracted into OM_X_arm and DX_XM430_W350. classes
     % Hopefully, you should only need what's in this class to accomplish everything.
     % But feel free to poke around!
     properties
         mDim; % Stores the robot link dimentions (mm)
         mOtherDim; % Stores extraneous second link dimensions (mm)
+        SList;
+        M;
+        MList;
+        GList;
+        eomg;
+        ev;
     end
 
     methods
@@ -15,9 +21,11 @@ classdef Robot < OM_X_arm
         % Super class constructor called implicitly
         % Add startup functionality here
         function self = Robot()
+            % gives us access to Modern-Robotics methods
+            addpath('../../ModernRobotics-master/packages/MATLAB/mr/');
             % Change robot to position mode with torque enabled by default
             % Feel free to change this as desired
-            self.writeMode('p');
+            self.writeMode('cp');
             self.writeMotorState(true);
 
             % Set the robot to move between positions with a 5 second profile
@@ -27,12 +35,74 @@ classdef Robot < OM_X_arm
             % Robot Dimensions
             self.mDim = [96.326, 130.23, 124, 133.4]; % (mm)
             self.mOtherDim = [128, 24]; % (mm)
-            self.mDHTable = [0 self.mDim(1) 0 -90;...
-                            (asind(self.mOtherDim(2)/self.mOtherDim(1)) - 90) 0 self.mDim(2) 0;...
-                            (90 - asind(self.mOtherDim(2)/self.mOtherDim(1))) 0 self.mDim(3) 0;...
-                            0 0 self.mDim(4) 0];
+
+            % Space-twist creation
+            S1 = [0; 0; 1;          0; 0;         0];
+            S2 = [0; 1; 0; -9.6326*10^-2; 0; 0];
+            S3 = [0; 1; 0; -2.24326*10^-1; 0; 2.4*10^-2];
+            S4 = [0; 1; 0; -2.24326*10^-1; 0; 1.48*10^-1];
+            self.SList = [S1,S2,S3,S4];
+            
+            % Home configuration
+            self.M = [  0, 0, -1,   2.814*10^-1;
+                        0, 1,  0,             0;
+                        1, 0,  0, 2.24326*10^-1;
+                        0, 0,  0,             1];
+            
+            % Mass-matrix list creation
+            M01 = [0.00, 0.00, 1.00, 0.00;
+                   0.00, -1.0, 0.00, 2.9*10^-4;
+                   1.00, 0.00, 0.00, 6.211*10^-2;
+                   0.00, 0.00, 0.00, 1.00];
+            
+            M02 = [0.00, 0.00, 1.00, 1.8*10^-3;
+                   0.00, -1.0, 0.00, -6.4*10^-4;
+                   1.00, 0.00, 0.00, 1.8968*10^-1;
+                   0.00, 0.00, 0.00, 1.00];
+
+            M03 = [1.00, 0.00, 0.00, 1.0698*10^-1;
+                   0.00, 1.00, 0.00, -4.6*10^-4;
+                   0.00, 0.00, 1.00, 2.2588*10^-1;
+                   0.00, 0.00, 0.00, 1.00];
+
+            M04 = [1.00, 0.00, 0.00, 2.1456*10^-1;
+                   0.00, 1.00, 0.00, -1.1*10^-4;
+                   0.00, 0.00, 1.00, 2.2803*10^-1;
+                   0.00, 0.00, 0.00, 1.00];
+
+            M12 = M01\M02;
+            M23 = M02\M03;
+            M34 = M03\M04;
+            M45 = M04\self.M;
+            
+            self.MList = cat(3,M01, M12, M23, M34, M45);
+            
+            % Spatia-inertia-matrix list creation
+            m1 = 1.185*10^-1;
+            m2 = 1.04*10^-1;
+            m3 = 8.3*10^-2;
+            m4 = 2.18*10^-1;
+
+            G1 = diag([4.055277*10^-5, 6.540397*10^-5, 6.664446*10^-5, m1,m1,m1]);
+            G2 = diag([3.848275*10^-5, 1.9010054*10^-4, 1.9540781*10^-4, m2,m2,m2]);
+            G3 = diag([2.255511*10^-5, 1.1750725*10^-4, 1.2792032*10^-4, m3,m3,m3]);
+            G4 = diag([1.8019833*10^-4, 2.0227701*10^-4, 2.8711671*10^-4, m4,m4,m4]);
+            
+            self.GList = cat(3,G1,G2,G3,G4);
+            self.eomg = 0.005;
+            self.ev = 0.01;
         end
 
+        function [angles, success] = IkinSpace501(self, T_des, T_guess)
+            % No T_guess given. Must generate a guess based on current position
+            if nargin == 2
+                q = self.getJointsReadings();
+                T_guess = transpose(deg2rad(q(1,:)));
+            end
+            [angles, success] = IKinSpace(self.SList, self.M, T_des, T_guess, self.eomg, self.ev);
+            angles = rad2deg(angles);
+        end
+        
         % Sends the joints to the desired angles
         % goals [1x4 double] - angles (degrees) for each of the joints to go to
         function writeJoints(self, goals)
---
+++ OpenManip_Skeleton-main/src/task1_r.m
@@ -0,0 +1,65 @@
+clear;
+clc;
+close all;
+
+%% Setup robot
+travelTime = 2; % Defines the travel time
+robot = Robot(); % Creates robot object
+robot.writeTime(travelTime); % Write travel time
+robot.writeMotorState(true); % Write position mode
+%% Program 
+
+robot.writeJoints(0); % Write joints to zero position
+pause(travelTime); % Wait for trajectory completion
+
+
+T_1 = [0, .71, -.71,  1.85*10^-1;
+       0, .71,  .71, -1.85*10^-1;
+       1,   0,    0,  1.85*10^-1;
+       0,   0,    0,           1];
+
+T_2 = [0, -.6766, -.73,  1.85*10^-1;
+       0, .73,  -.6766,  1.70*10^-1;
+       1,   0,    0,   7.0*10^-2;
+       0,   0,    0,           1];
+
+T_3 = [0, 0, -1,  1.85*10^-1;
+       0, 1, 0,  0;
+       1, 0, 0,  2.4*10^-1;
+       0, 0, 0,           1];
+
+Places=cat(3,T_1,T_3, T_2);
+
+theta0_1 = [-0.78;0;0;0];
+theta0_2 = [0.7432;0;0;0];
+theta0_3 = [0;0;0;0];
+guesses = [theta0_1, theta0_3, theta0_2];
+j2_current = zeros(1, 1000);
+t_values =   zeros(1, 1000);
+iter = 1;
+
+% w0 =  [0,0,0,0];
+% w1 = [-44.7, -8.8, 28.74, -19.88];
+% w2 = [42.8, 33.06, 44.3, -77.36];
+% w3 = [      0,  -44.7606,   28.1950,   16.5656];
+% waypoints = [w0;w1;w3;w2];
+
+for i = 1:size(Places,3) % Iterate through waypoints
+    [angles, success] = robot.IkinSpace501(Places(:,:,i), guesses(:,i));
+    disp(angles)
+    robot.writeJoints(angles)
+    tic; % Start timer
+    % iter = 1;
+    while toc < travelTime
+       
+        jvs = robot.getJointsReadings(); % Read joint values
+        t_values(iter) = toc+(i-1)*travelTime;
+        j2_current(iter) = jvs(2,2);
+        iter = iter + 1;
+    end
+end
+
+plot(t_values(1:(iter-1)),j2_current(1:(iter-1)))
+title("Current (mA) of joint 2 vs Time (s)")
+
+
