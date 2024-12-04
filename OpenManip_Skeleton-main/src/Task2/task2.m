e4f71adca14257ffef859d20859426fff6c848b6
John Hall <jkhall@wpi.edu>
2024-12-04 15:00:15 -0500

    LSPB first draft

--- OpenManip_Skeleton-main/src/Task2/task2.m
+++ OpenManip_Skeleton-main/src/Task2/task2.m
@@ -3,7 +3,7 @@ clc;
 close all;
 
 %% Setup robot
-travelTime = 1.25; % Defines the travel time
+travelTime = 2; % Defines the travel time
 robot = Robot(); % Creates robot object
 robot.writeTime(travelTime); % Write travel time
 robot.writeMotorState(true); % Write position mode
@@ -11,6 +11,9 @@ robot.writeMotorState(true); % Write position mode
 
 robot.writeJoints(0); % Write joints to zero position
 pause(travelTime); % Wait for trajectory completion
+setPointTravelTime = 0.005;
+robot.writeTime(0.005);
+tf = 10;
 
 
 T_1 = [0, .71, -.71,  1.85*10^-1;
@@ -34,7 +37,7 @@ theta0_1 = [-0.78;0;0;0];
 theta0_2 = [0.7432;0;0;0];
 theta0_3 = [0;0;0;0];
 guesses = [theta0_1, theta0_3, theta0_2];
-j2_current = zeros(1, 1000);
+j_current = zeros(4, 1000);
 t_values =   zeros(1, 1000);
 iter = 1;
 
@@ -43,18 +46,25 @@ iter = 1;
 % w2 = [42.8, 33.06, 44.3, -77.36];
 % w3 = [      0,  -44.7606,   28.1950,   16.5656];
 % waypoints = [w0;w1;w3;w2];
-
+% path = LSBP();
+V = 10; %deg/s
 for i = 1:size(Places,3) % Iterate through waypoints
-    [angles, success] = robot.IkinSpace501(Places(:,:,i), guesses(:,i));
-    disp(angles)
-    robot.writeJoints(angles)
-    tic; % Start timer
-    % iter = 1;
-    while toc < travelTime
-       
+    jvs = robot.getJointsReadings();
+    q0 = jvs(1,:);
+    [qf, success] = robot.IkinSpace501(Places(:,:,i), guesses(:,i));
+    disp(qf)
+    
+    path = LSPB(q0,qf,tf,V);
+    
+     % Start timer
+    tic;
+    while toc < tf
+        [q,qdot] = path.curr_increment(toc);
         jvs = robot.getJointsReadings(); % Read joint values
+        robot.writeJoints(transpose(q));
+        disp(q)
         t_values(iter) = toc+(i-1)*travelTime;
-        j2_current(iter) = jvs(2,2);
+        j_current(:,iter) = jvs(1,:);
         iter = iter + 1;
     end
 end
