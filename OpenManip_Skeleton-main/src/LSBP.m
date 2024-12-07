d5d3455ab74963453f39113081d1480511b5f73f
John Hall <jkhall@wpi.edu>
2024-12-07 10:35:33 -0500

    LSPB error checked

--- OpenManip_Skeleton-main/src/LSBP.m
+++ OpenManip_Skeleton-main/src/LSBP.m
@@ -12,7 +12,7 @@ classdef LSBP
     end
     
     methods
-        function self = LSBP(self,q0,qf,tf,V)
+        function self = LSBP(q0,qf,tf,V)
             temp_tb = zeros(4:1);
             temp_alpha = zeros(4:1);
             for i = 1:4
@@ -24,6 +24,7 @@ classdef LSBP
             self.V = V;
             self.alpha = temp_alpha;
             self.q0 = q0;
+            self.qf = qf;
             disp(self.tb)
             disp(self.alpha)
         end
@@ -33,15 +34,15 @@ classdef LSBP
             qdot = zeros(4,1);
             for i = 1:4
                 if t<=self.tb(i)
-                    q(i) = self.q0(i) + self.alpha/2*power(t,2);
-                    qdot(i) = self.alpha*t;
+                    q(i) = self.q0(i) + self.alpha(i)/2*power(t,2);
+                    qdot(i) = self.alpha(i)*t;
                 
                 elseif t <= self.tf - self.tb(i)
                     q(i) = (self.qf(i)+self.q0(i)-self.V*self.tf)/2 + self.V*t;
                     qdot(i) = self.V;
                 else
-                    q(i) = self.qf(i) - self.alpha/2*power(t,2) - self.alpha/2*power(self.tf,2) + self.alpha*self.tf*t;
-                    qdot(i) = self.V-self.alpha*t;
+                    q(i) = self.qf(i) - self.alpha(i)/2*power(t,2) - self.alpha(i)/2*power(self.tf,2) + self.alpha(i)*self.tf*t;
+                    qdot(i) = self.V-self.alpha(i)*(t-(self.tf-self.tb(i)));
                 end
             end
         end
