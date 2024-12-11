0b67529014db31b9089a912a4f24cb0c58a52b92
John Hall <jkhall@wpi.edu>
2024-12-11 14:13:24 -0500

    torque graphing

--- OpenManip_Skeleton-main/src/Task1/task1_final.m
+++ OpenManip_Skeleton-main/src/Task1/task1_final.m
@@ -3,7 +3,7 @@ clc;
 close all;
 
 %% Setup robot
-travelTime = 1.25; % Defines the travel time
+travelTime = 2; % Defines the travel time
 robot = Robot(); % Creates robot object
 robot.writeTime(travelTime); % Write travel time
 robot.writeMotorState(true); % Write position mode
@@ -28,13 +28,14 @@ T_3 = [0, 0, -1,  1.85*10^-1;
        1, 0, 0,  2.4*10^-1;
        0, 0, 0,           1];
 
-Places=cat(3,T_1,T_3, T_2);
+Places=cat(2,T_1, T_2);
 
 theta0_1 = [-0.78;0;0;0];
 theta0_2 = [0.7432;0;0;0];
 theta0_3 = [0;0;0;0];
 guesses = [theta0_1, theta0_3, theta0_2];
-j2_current = zeros(1, 1000);
+j_torques_guess = zeros(4, 1000);
+j_torques_inv = zeros(4,1000);
 t_values =   zeros(1, 1000);
 iter = 1;
 
@@ -43,19 +44,33 @@ iter = 1;
 % w2 = [42.8, 33.06, 44.3, -77.36];
 % w3 = [      0,  -44.7606,   28.1950,   16.5656];
 % waypoints = [w0;w1;w3;w2];
+robot.writeJoints([-44.7, -8.8, 28.74, -19.88]);
+pause(travelTime);
 
-for i = 1:size(Places,3) % Iterate through waypoints
-    [angles, success] = robot.IkinSpace501(Places(:,:,i), guesses(:,i));
-    disp(angles)
-    robot.writeJoints(angles)
-    tic; % Start timer
-    % iter = 1;
-    while toc < travelTime
-       
-        jvs = robot.getJointsReadings(); % Read joint values
-        t_values(iter) = toc+(i-1)*travelTime;
-        j2_current(iter) = jvs(2,2);
-        iter = iter + 1;
-    end
+[angles, success] = robot.IkinSpace501(T_2, theta0_2);
+disp(angles)
+robot.writeJoints(angles)
+
+tic; % Start timer
+% iter = 1;
+jvs = robot.getJointsReadings();
+prev_j_vels = transpose(jvs(2,:));
+prev_toc = zeros(4,1);
+while toc < travelTime
+   
+    jvs = robot.getJointsReadings(); % Read joint values
+    t_values(iter) = toc;
+    j_angles = transpose(jvs(1,:));
+    j_vels = transpose(jvs(2,:));
+    j_accs = (j_vels-prev_j_vels)./(toc-prev_toc);
+    j1 = [j_angles(1),j_vels(1),j_accs(1)];
+    disp(j1)
+    j_current = transpose(jvs(3,:));
+    j_torques_guess(:,iter) = j_current.*0.0045;
+    j_torques_inv(:,iter) = InverseDynamics(deg2rad(j_angles),deg2rad(j_vels),deg2rad(j_accs),[0;0;-9.8],[0;0;0;0;0;0],robot.MList, robot.GList, robot.SList);
+    iter = iter + 1;
+
+    prev_j_vels = j_vels;
 end
 
+save("Task1/Task_1_Torques", "j_torques_guess", "j_torques_inv" ,"iter", "t_values")
---
+++ OpenManip_Skeleton-main/src/Task1/task1_torque_graphing.m
@@ -0,0 +1,25 @@
+clc;
+clear;
+close all;
+
+load Task_1_Torques.mat
+j_t_g = j_torques_guess;
+j_t_i = j_torques_inv;
+j_10_t = t_values;
+j_10_i = iter;
+
+figure;
+hold on;
+grid on;
+plot(j_10_t(1:(j_10_i-1)),j_t_g(1,1:(j_10_i-1)), 'DisplayName', 'Joint 1 Torque Current Estimate', "Color", "red")
+plot(j_10_t(1:(j_10_i-1)),j_t_i(1,1:(j_10_i-1)), 'DisplayName', 'Joint 1 Torque Inverse Dynamics', "Color", "red", 'LineStyle','--')
+plot(j_10_t(1:(j_10_i-1)),j_t_g(2,1:(j_10_i-1)), 'DisplayName', 'Joint 2 Torque Current Estimate', "Color", "green")
+plot(j_10_t(1:(j_10_i-1)),j_t_i(2,1:(j_10_i-1)), 'DisplayName', 'Joint 2 Torque Inverse Dynamics', "Color", "green", 'LineStyle','--')
+plot(j_10_t(1:(j_10_i-1)),j_t_g(3,1:(j_10_i-1)), 'DisplayName', 'Joint 3 Torque Current Estimate', "Color", "blue")
+plot(j_10_t(1:(j_10_i-1)),j_t_i(3,1:(j_10_i-1)), 'DisplayName', 'Joint 3 Torque Inverse Dynamics', "Color", "blue", 'LineStyle','--')
+plot(j_10_t(1:(j_10_i-1)),j_t_g(4,1:(j_10_i-1)), 'DisplayName', 'Joint 4 Torque Current Estimate', "Color", "black")
+plot(j_10_t(1:(j_10_i-1)),j_t_i(4,1:(j_10_i-1)), 'DisplayName', 'Joint 4 Torque Inverse Dynamics', "Color", "black", 'LineStyle','--')
+hold off;
+xlabel("Time (s)")
+ylabel("Joint Torque")
+legend
