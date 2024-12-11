5fbf5d504f8bb432628388a0ddffdd36aac95a74
riblair <rileypatblair@gmail.com>
2024-12-11 13:36:14 -0500

    new graphing for task 3

--- OpenManip_Skeleton-main/src/Task3/task3_graphing.m
+++ OpenManip_Skeleton-main/src/Task3/task3_graphing.m
@@ -10,7 +10,7 @@ j_10_i = iter;
 j_10_q_des = j_angles_des;
 j_10_qdot_des = j_velos_des;
 
-load Task3DataStiched2sec.mat
+load Task3DataStiched5sec.mat
 j_2_q = j_angles;
 j_2_qdot = j_velos;
 j_2_t = t_values;
@@ -37,14 +37,14 @@ legend
 
 subplot(1,2,2);
 hold on;
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot(1,1:(j_10_i-1)), 'DisplayName', 'Joint 1 Angle', "Color", "red")
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(1,1:(j_10_i-1)), 'DisplayName', 'Joint 1 Angle Des', "Color", "red", 'LineStyle','--')
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot(2,1:(j_10_i-1)), 'DisplayName', 'Joint 2 Angle', "Color", "green")
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(2,1:(j_10_i-1)), 'DisplayName', 'Joint 2 Angle Des', "Color", "green", 'LineStyle','--')
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot(3,1:(j_10_i-1)), 'DisplayName', 'Joint 3 Angle', "Color", "blue")
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(3,1:(j_10_i-1)), 'DisplayName', 'Joint 3 Angle Des', "Color", "blue", 'LineStyle','--')
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot(4,1:(j_10_i-1)), 'DisplayName', 'Joint 4 Angle', "Color", "black")
-plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(4,1:(j_10_i-1)), 'DisplayName', 'Joint 4 Angle Des', "Color", "black", 'LineStyle','--')
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot(1,1:(j_10_i-1)), 'DisplayName', 'Joint 1 Velocity', "Color", "red")
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(1,1:(j_10_i-1)), 'DisplayName', 'Joint 1 Velocity Des', "Color", "red", 'LineStyle','--')
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot(2,1:(j_10_i-1)), 'DisplayName', 'Joint 2 Velocity', "Color", "green")
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(2,1:(j_10_i-1)), 'DisplayName', 'Joint 2 Velocity Des', "Color", "green", 'LineStyle','--')
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot(3,1:(j_10_i-1)), 'DisplayName', 'Joint 3 Velocity', "Color", "blue")
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(3,1:(j_10_i-1)), 'DisplayName', 'Joint 3 Velocity Des', "Color", "blue", 'LineStyle','--')
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot(4,1:(j_10_i-1)), 'DisplayName', 'Joint 4 Velocity', "Color", "black")
+plot(j_10_t(1:(j_10_i-1)),j_10_qdot_des(4,1:(j_10_i-1)), 'DisplayName', 'Joint 4 Velocity Des', "Color", "black", 'LineStyle','--')
 hold off;
 xlabel("Time (s)")
 ylabel("Joint Velocity (degree/sec)")
@@ -70,16 +70,17 @@ legend
 
 subplot(1,2,2);
 hold on;
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot(1,1:(j_2_i-1)), 'DisplayName', 'Joint 1 Angle', "Color", "red")
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(1,1:(j_2_i-1)), 'DisplayName', 'Joint 1 Angle Des', "Color", "red", 'LineStyle','--')
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot(2,1:(j_2_i-1)), 'DisplayName', 'Joint 2 Angle', "Color", "green")
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(2,1:(j_2_i-1)), 'DisplayName', 'Joint 2 Angle Des', "Color", "green", 'LineStyle','--')
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot(3,1:(j_2_i-1)), 'DisplayName', 'Joint 3 Angle', "Color", "blue")
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(3,1:(j_2_i-1)), 'DisplayName', 'Joint 3 Angle Des', "Color", "blue", 'LineStyle','--')
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot(4,1:(j_2_i-1)), 'DisplayName', 'Joint 4 Angle', "Color", "black")
-plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(4,1:(j_2_i-1)), 'DisplayName', 'Joint 4 Angle Des', "Color", "black", 'LineStyle','--')
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot(1,1:(j_2_i-1)), 'DisplayName', 'Joint 1 Velocity', "Color", "red")
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(1,1:(j_2_i-1)), 'DisplayName', 'Joint 1 Velocity Des', "Color", "red", 'LineStyle','--')
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot(2,1:(j_2_i-1)), 'DisplayName', 'Joint 2 Velocity', "Color", "green")
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(2,1:(j_2_i-1)), 'DisplayName', 'Joint 2 Velocity Des', "Color", "green", 'LineStyle','--')
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot(3,1:(j_2_i-1)), 'DisplayName', 'Joint 3 Velocity', "Color", "blue")
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(3,1:(j_2_i-1)), 'DisplayName', 'Joint 3 Velocity Des', "Color", "blue", 'LineStyle','--')
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot(4,1:(j_2_i-1)), 'DisplayName', 'Joint 4 Velocity', "Color", "black")
+plot(j_2_t(1:(j_2_i-1)),j_2_qdot_des(4,1:(j_2_i-1)), 'DisplayName', 'Joint 4 Velocity Des', "Color", "black", 'LineStyle','--')
+
 hold off;
 xlabel("Time (s)")
 ylabel("Joint Velocity (degree/sec)")
 legend
-sgtitle("Joint Angle and Velocities vs Time (LSPB w/ 2sec Travel Time)")
+sgtitle("Joint Angle and Velocities vs Time (LSPB w/ 5sec Travel Time)")
